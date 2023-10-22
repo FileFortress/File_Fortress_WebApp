f10d91f74c5b669a768da5f48766a4ce40bc242b
Nuyun Kalamullage <73055937+Nuyun-Kalamullage@users.noreply.github.com>
2023-10-23 01:21:06 +0530

    setup simple user/admin login and logout with different roles. Implemented the side Navigation bar in the web app and placed the logo, application name, and user profile. create UI components for the Setup Exam section on the Examiner end.

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-form-layout-47744b1d-e2184e4a.js
@@ -0,0 +1,1 @@
+import{R as o}from"./indexhtml-05309a64.js";const r={tagName:"vaadin-form-layout",displayName:"Form Layout",elements:[{selector:"vaadin-form-layout",displayName:"Layout",properties:[o.backgroundColor,o.borderColor,o.borderWidth,o.borderRadius,o.padding]}]};export{r as default};
