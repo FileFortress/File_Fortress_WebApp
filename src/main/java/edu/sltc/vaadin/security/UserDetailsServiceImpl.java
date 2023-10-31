1a15dc5b077157d96b72588806119a2da682a9bb
Nuyun Kalamullage <73055937+Nuyun-Kalamullage@users.noreply.github.com>
2023-10-31 20:22:07 +0530

    Tried out firebase intergration to the web app.

--- src/main/java/edu/sltc/vaadin/security/UserDetailsServiceImpl.java
+++ src/main/java/edu/sltc/vaadin/security/UserDetailsServiceImpl.java
@@ -11,6 +11,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.google.firebase.auth.FirebaseToken;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinServletRequest;
+import java.util.Optional;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -37,6 +46,26 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                 .collect(Collectors.toList());
 
+    }
+    public Optional<Authentication> getAuthentication() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        return Optional.ofNullable(context.getAuthentication())
+                .filter(authentication -> !(authentication instanceof AnonymousAuthenticationToken));
+    }
+    public Optional<FirebaseToken> get() {
+        Optional<Authentication> authentication = getAuthentication();
+        if(authentication.isEmpty()) {
+            return Optional.empty();
+        } else {
+            FirebaseToken token = (FirebaseToken) authentication.get().getDetails();
+            return Optional.ofNullable(token);
+        }
+    }
+
+    public void logout() {
+        UI.getCurrent().getPage().setLocation(SecurityConfiguration.LOGOUT_URL);
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(VaadinServletRequest.getCurrent().getHttpServletRequest(), null, null);
     }
 
 }
