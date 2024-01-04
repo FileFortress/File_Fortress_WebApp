a7daa2bcd79134eb802ff543867a1f71ab3f4ad8
Nuyun Kalamullage <73055937+Nuyun-Kalamullage@users.noreply.github.com>
2024-01-04 22:18:18 +0530

    I have changes in fileUploader.js and clientKeyExchange.js files

--- frontend/clientKeyExchange.js
+++ frontend/clientKeyExchange.js
@@ -1,5 +1,18 @@
 window.getServerPublic = function  getServerPublic(key){
         console.log('Server Public JS part Executed');
         console.log("Server send a Key: ", key);
-        localStorage.setItem("serverPublic", key);
+        sessionStorage.setItem("serverPublic", key);
+}
+// window.sendClientPublic = function sendClientPublic(view) {
+//        // view; // I want save this ui layout to call view.$server.setClientPublicKey(); this in other js class
+// }
+let mainView;
+window.key = {
+        init: function (view) {
+                mainView = view.$server;
+                mainView.setClientPublicKey("Init method invoking");
+        },
+        sendClientPublic: function (publicKey){
+                mainView.setClientPublicKey(publicKey);
+        }
 }
